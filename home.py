b3c3c8ed40d2b56027bd8a3f193fe7b545f78333
devanshgurawa <130561264+devanshgurawa@users.noreply.github.com>
2023-05-08 16:47:11 +0530

    Add files via upload

---
+++ home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+def app():
+    st.header('Car price prediction app')
+    st.text('''This Web app allows the user to predict the prices of a car based on the engine size, car's power, dimensions and drive wheel''')
+
+
